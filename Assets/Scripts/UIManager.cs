f997af10e98a3f9d5cdf1a6c2a28e545b150461b
XardoX <jurek.gab.jg@gmail.com>
2023-10-10 11:51:42 +0200

    hearths in ui

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Image> hearths = new();
+
+    public void SetHearths(int count)
+    {
+        foreach(var hearth in hearths)
+        {
+            hearth.gameObject.SetActive(false);
+        }
+
+        for(int i = 0; i < hearths.Count && i < count; i++)
+        {
+            hearths[i].gameObject.SetActive(true);
+        }
+        
+    }
+}
