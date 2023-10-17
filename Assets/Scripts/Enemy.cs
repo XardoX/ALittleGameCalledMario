d1ffce41e773b51ccba768f1616e7222e4b5137e
XardoX <jurek.gab.jg@gmail.com>
2023-10-17 11:25:37 +0200

    enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,32 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 10;
+    [SerializeField]
+    private LayerMask layerMask;
+    [SerializeField]
+    private float rayDistance = 1f;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    private Vector3 moveDirection = Vector2.right;
 
     private void Update()
     {
-        var dir = Vector3.right;
-        transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        if(Physics2D.Raycast(transform.position, moveDirection,rayDistance,layerMask))
+        {
+            moveDirection *= -1;
+
+            spriteRenderer.flipX = moveDirection.x > 0 ? true : false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
